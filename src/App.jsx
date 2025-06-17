4410662ae20a4e89b21d46dbed3a1d68243f7d80
Ishan Ghosal <ishanghoshal05@gmail.com>
2025-06-17 11:43:33 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>ishan nigga</h1>
+      <h1>ishan super nigga</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
